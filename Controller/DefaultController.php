4425a2a2d86f2f8b382cade58d450ebeff8d1604
Jakub Matejka <jakub@keboola.com>
2014-09-29 11:37:27 +0200

    feat: init

---
+++ Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Keboola\SklikExtractorBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('KeboolaSklikExtractorBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ KeboolaSklikExtractorBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Keboola\SklikExtractorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KeboolaSklikExtractorBundle extends Bundle
+{
+}
