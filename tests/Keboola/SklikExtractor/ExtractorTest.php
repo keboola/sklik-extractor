38d0d470565243ba24dc68b76fe3f90d9d9f5bcd
Petr Daňhel <Petr Dahel>
2016-11-08 09:28:29 +0100

    fix last wrong test

--- tests/Keboola/SklikExtractor/ExtractorTest.php
+++ tests/Keboola/SklikExtractor/ExtractorTest.php
@@ -18,7 +18,30 @@ class ExtractorTest extends AbstractTest
         $endDate = \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d 00:00:01', strtotime('-1 day')));
 
         $e = new Extractor(EX_SK_USERNAME, EX_SK_PASSWORD, sys_get_temp_dir(), 'out.c-main', EX_SK_API_URL);
-        $e->run($startDate, $endDate);
+        $e->run($startDate, $endDate, false);
+
+        $this->assertFileExists(sys_get_temp_dir().'/out.c-main.accounts.csv');
+        $fp = file(sys_get_temp_dir().'/out.c-main.accounts.csv');
+        $this->assertGreaterThan(1, count($fp));
+
+        $this->assertFileExists(sys_get_temp_dir().'/out.c-main.campaigns.csv');
+        $fp = file(sys_get_temp_dir().'/out.c-main.campaigns.csv');
+        $this->assertGreaterThan(1, count($fp));
+
+        $this->assertFileExists(sys_get_temp_dir().'/out.c-main.stats.csv');
+        $fp = file(sys_get_temp_dir().'/out.c-main.stats.csv');
+        $this->assertGreaterThan(1, count($fp));
+    }
+
+    public function testExtractionWithShareImpressions()
+    {
+        $this->createCampaign(uniqid());
+
+        $startDate = \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d 00:00:01', strtotime('-2 days')));
+        $endDate = \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d 00:00:01', strtotime('-1 day')));
+
+        $e = new Extractor(EX_SK_USERNAME, EX_SK_PASSWORD, sys_get_temp_dir(), 'out.c-main', EX_SK_API_URL);
+        $e->run($startDate, $endDate, true);
 
         $this->assertFileExists(sys_get_temp_dir().'/out.c-main.accounts.csv');
         $fp = file(sys_get_temp_dir().'/out.c-main.accounts.csv');
