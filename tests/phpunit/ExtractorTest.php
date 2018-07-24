60ceb89d80d5968dbd17459c6257f5a5538668a4
Jakub Matejka <jakub@keboola.com>
2018-07-24 20:56:34 +0200

    fix: add query instad of id to display columns of queries resource

--- tests/phpunit/ExtractorTest.php
+++ tests/phpunit/ExtractorTest.php
@@ -51,6 +51,16 @@ class ExtractorTest extends TestCase
                         'displayOptions' => json_encode(['statGranularity' => 'daily']),
                         'displayColumns' => 'name, clicks, impressions, budget.name',
                     ],
+                    [
+                        'name' => 'queries',
+                        'resource' => 'queries',
+                        'restrictionFilter' => json_encode([
+                            'dateFrom' => getenv('SKLIK_DATE_FROM'),
+                            'dateTo' => getenv('SKLIK_DATE_TO'),
+                        ]),
+                        'displayOptions' => json_encode(['statGranularity' => 'daily']),
+                        'displayColumns' => 'query,group.name,keyword.id',
+                    ],
                 ],
             ],
         ], new ConfigDefinition());
@@ -59,11 +69,14 @@ class ExtractorTest extends TestCase
 
         $this->assertFileExists($this->temp->getTmpFolder() . '/accounts.csv');
         $this->assertFileExists($this->temp->getTmpFolder() . '/report1.csv');
+        $this->assertFileExists($this->temp->getTmpFolder() . '/queries.csv');
         $metaFile = file($this->temp->getTmpFolder() . '/report1.csv');
         $this->assertEquals('"id","accountId","budget_name","name"', trim($metaFile[0]));
         $this->assertFileExists($this->temp->getTmpFolder() . '/report1-stats.csv');
         $statsFile = file($this->temp->getTmpFolder() . '/report1-stats.csv');
         $this->assertEquals('"id","clicks","date","impressions"', trim($statsFile[0]));
+        $metaFile = file($this->temp->getTmpFolder() . '/queries.csv');
+        $this->assertEquals('"query","accountId","group_name","keyword_id"', trim($metaFile[0]));
     }
 
     /**
