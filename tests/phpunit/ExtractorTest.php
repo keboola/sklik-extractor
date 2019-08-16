ea4a84507e3e251b5341d069683215501becd2f9
odin <odinuv@gmail.com>
2019-08-16 12:15:24 +0200

    tests: failing test for empty display options

--- tests/phpunit/ExtractorTest.php
+++ tests/phpunit/ExtractorTest.php
@@ -144,4 +144,48 @@ class ExtractorTest extends TestCase
         $this->assertArrayHasKey('dateFrom', $reports[0]['restrictionFilter']);
         $this->assertEquals('-1 day', $reports[0]['restrictionFilter']['dateFrom']);
     }
+
+    public function testExtractorRunNoDisplayOptions(): void
+    {
+        $config = new Config([
+            'parameters' => [
+                '#token' => getenv('SKLIK_API_TOKEN'),
+                'reports' => [
+                    [
+                        'name' => 'report1',
+                        'resource' => 'campaigns',
+                        'restrictionFilter' => json_encode([
+                            'dateFrom' => getenv('SKLIK_DATE_FROM'),
+                            'dateTo' => getenv('SKLIK_DATE_TO'),
+                        ]),
+                        'displayOptions' => json_encode([]),
+                        'displayColumns' => 'name, clicks, impressions, budget.name',
+                    ],
+                    [
+                        'name' => 'queries',
+                        'resource' => 'queries',
+                        'restrictionFilter' => json_encode([
+                            'dateFrom' => getenv('SKLIK_DATE_FROM'),
+                            'dateTo' => getenv('SKLIK_DATE_TO'),
+                        ]),
+                        'displayOptions' => json_encode(['statGranularity' => 'daily']),
+                        'displayColumns' => 'query,group.name,keyword.id',
+                    ],
+                ],
+            ],
+        ], new ConfigDefinition());
+
+        $this->extractor->run($config);
+
+        $this->assertFileExists($this->temp->getTmpFolder() . '/accounts.csv');
+        $this->assertFileExists($this->temp->getTmpFolder() . '/report1.csv');
+        $this->assertFileExists($this->temp->getTmpFolder() . '/queries.csv');
+        $metaFile = file($this->temp->getTmpFolder() . '/report1.csv');
+        $this->assertEquals('"id","accountId","budget_name","name"', trim($metaFile[0]));
+        $this->assertFileExists($this->temp->getTmpFolder() . '/report1-stats.csv');
+        $statsFile = file($this->temp->getTmpFolder() . '/report1-stats.csv');
+        $this->assertEquals('"id","clicks","date","impressions"', trim($statsFile[0]));
+        $metaFile = file($this->temp->getTmpFolder() . '/queries.csv');
+        $this->assertEquals('"query","accountId","group_name","keyword_id"', trim($metaFile[0]));
+    }
 }
