6ff27ae2c4f7d90fe1e1c66565e7d76ff560fe6f
Erik Zigo <erik.zigo@keboola.com>
2024-09-09 14:03:16 +0200

    testRetryOnReportReadWithInternalErrorHavingSuccessHTTPCode

--- tests/phpunit/SklikApiTest.php
+++ tests/phpunit/SklikApiTest.php
@@ -166,6 +166,8 @@ class SklikApiTest extends TestCase
 
         $this->api->loginByToken(getenv('SKLIK_API_TOKEN'));
 
+        self::assertFalse($this->testHandler->hasErrorRecords());
+
         $createReportResponse = $this->api->createReport(
             'campaigns',
             [
@@ -177,6 +179,57 @@ class SklikApiTest extends TestCase
 
         self::assertArrayHasKey('reportId', $createReportResponse);
         self::assertSame('1234567890', $createReportResponse['reportId']);
+
+        self::assertTrue($this->testHandler->hasError(
+            'API Error, will be retried. Retry count: 1x',
+        ));
+
+        self::assertTrue($this->testHandler->hasError(
+            'API Error, will be retried. Retry count: 5x',
+        ));
+    }
+
+    public function testRetryOnReportReadWithInternalErrorHavingSuccessHTTPCode(): void
+    {
+        $this->testHandler->clear();
+
+        $this->api = new SklikApi(
+            $this->logger,
+            getenv('SKLIK_API_URL') ?: null,
+            HandlerStack::create(new MockHandler($this->getResponseForTestRetryOnReportReadWithInternalErrorHavingSuccessHTTPCode())),
+        );
+
+        $this->api->loginByToken(getenv('SKLIK_API_TOKEN'));
+
+        $createReportResponse = $this->api->createReport(
+            'campaigns',
+            [
+                'dateFrom' => getenv('SKLIK_DATE_FROM'),
+                'dateTo' => getenv('SKLIK_DATE_TO'),
+            ],
+            ['statGranularity' => 'daily'],
+        );
+
+        self::assertFalse($this->testHandler->hasErrorRecords());
+
+        self::assertArrayHasKey('reportId', $createReportResponse);
+        $readReportResponse = $this->api->readReport(
+            'campaigns',
+            $createReportResponse['reportId'],
+            true,
+            ['id', 'name', 'clicks', 'impressions'],
+        );
+
+        self::assertSame(
+            [
+                'stats' => [
+                    [
+                        'day' => 'none',
+                    ],
+                ],
+            ],
+            $readReportResponse
+        );
 
         self::assertTrue($this->testHandler->hasError(
             'API Error, will be retried. Retry count: 1x',
@@ -201,6 +254,27 @@ class SklikApiTest extends TestCase
 
         $responses[] = $loginResponse;
         $responses[] = new Response(200, [], '{"status":200,"statusMessage":"OK","session":"dummySessionValue","reportId":"1234567890","totalCount":1}');
+
+        return $responses;
+    }
+
+    /** @return Response[] */
+    private function getResponseForTestRetryOnReportReadWithInternalErrorHavingSuccessHTTPCode(): array
+    {
+        $responses = [];
+
+        $loginResponse = new Response(200, [], '{"status":200,"statusMessage":"OK","session":"dummySessionValue"}');
+
+        $responses[] = $loginResponse;
+        $responses[] = new Response(200, [], '{"status":200,"statusMessage":"OK","session":"dummySessionValue","reportId":"1234567890","totalCount":1}');
+
+        for ($x = 0; $x < SklikApi::RETRY_MAX_ATTEMPTS; $x++) {
+            $responses[] = new Response(200, [], '{"status":"error","message":"Server error","code":500}');
+            $responses[] = $loginResponse;
+
+        }
+
+        $responses[] = new Response(200, [], '{"status":200,"statusMessage":"OK","session":"dummySessionValue","report":{"stats":[{"day":"none"}]}}');
 
         return $responses;
     }
